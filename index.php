4c731cc81362ccd769aeeaa8a3c4479f1a446abf
jadefrh <jadefrh@gmail.com>
2016-04-13 18:18:36 +0200

    conflict fixed

--- index.php
+++ index.php
@@ -14,21 +14,17 @@ $q = empty($_GET['q']) ? '' : $_GET['q'];
 
 // Routes
 if($q == '')
-$page = 'home';
+	$page = 'home';
 else if($q == 'about')
-<<<<<<< Updated upstream
-$page = 'about';
-else if($q == 'news')
-$page = 'news';
-=======
 	$page = 'about';
+else if($q == 'news')
+	$page = 'news';
 else if($q == 'results')
 	$page = 'results';
->>>>>>> Stashed changes
 else if(preg_match('/^news\/[-a-z0-9]+$/',$q)) // news/mon-titre-d-actualite
-$page = 'news-single';
+	$page = 'news-single';
 else
-$page = '404';
+	$page = '404';
 
 // Includes
 include 'controllers/'.$page.'.php';
@@ -40,10 +36,7 @@ include 'views/partials/footer.php';
 require_once('class/simple_html_dom.php');
 require_once('class/googleshowtime.php');
 
-<<<<<<< Updated upstream
-// The Movie Database
-require_once('class/themoviedb.php');
-=======
+
 date_default_timezone_set('Europe/Paris');
 $date_hour = date('H', time());
 $date_minutes = date('i', time());
@@ -53,4 +46,4 @@ echo "<pre>";
 var_dump($_SESSION["showtimes"]);
 echo "</pre>";
 ?>
->>>>>>> Stashed changes
+
